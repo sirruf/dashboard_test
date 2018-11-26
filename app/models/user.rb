9721cf2d9ade3f77cdeff8022d68997db27b91ab
Artem Kolesnikov <sirruf@me.com>
2018-11-26 23:56:10 +0300

    Relations with Dashboard added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :dashboard
+  has_many :dashboard_elements, through: :dashboard
+
+  def default_dashboard
+    Dashboard.find_or_create_by(user: self)
+  end
 end
